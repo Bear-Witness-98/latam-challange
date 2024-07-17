8e5831e6dc9920a51acbaf2ea48356f78d03f179
Santiago Suarez <santiago@tryolabs.com>
2024-07-17 14:16:28 -0300

    Additional model improvements

--- challenge/model.py
+++ challenge/model.py
@@ -1,3 +1,5 @@
+import os
+import pickle
 from datetime import datetime
 from typing import List, Optional, Tuple, Union
 
@@ -31,7 +33,7 @@ def get_delay_target(data: pd.DataFrame) -> pd.Series:
     threshold_in_minutes = 15
     data["delay"] = np.where(data["min_diff"] > threshold_in_minutes, 1, 0)
 
-    return data["delay"]
+    return data["delay"].to_frame()
 
 
 def get_features(data: pd.DataFrame) -> pd.DataFrame:
@@ -44,14 +46,21 @@ def get_features(data: pd.DataFrame) -> pd.DataFrame:
         ],
         axis=1,
     )
+    present_features = list(set(FEATURES_COLS).intersection(set(features.columns)))
+    missing_features = list(set(FEATURES_COLS).difference(set(features.columns)))
 
-    features = features[FEATURES_COLS]
+    features = features[present_features]
+
+    # fill missing features with 0 due to one-hot encoding of features
+    for feature in missing_features:
+        features[feature] = 0
+
     return features
 
 
 class DelayModel:
     def __init__(self):
-        self._model = self._model = LogisticRegression()
+        self._model = LogisticRegression()
 
     def preprocess(
         self, data: pd.DataFrame, target_column: Optional[str] = None
@@ -75,7 +84,7 @@ class DelayModel:
         if target_column is None:
             return x
         elif target_column == "delay":
-            y = get_delay_target(data).to_frame()
+            y = get_delay_target(data)
             return (x, y)
         else:
             raise NotImplementedError("Only implemented 'delay' as target column")
@@ -104,6 +113,21 @@ class DelayModel:
         # for scikitlearn compatibility
         return self
 
+    def save_model(self, path: str):
+        """
+        Store trained model in given path
+        """
+        os.makedirs(path, exist_ok=True)
+        with open(f"{path}/model.pkl", "wb") as f:
+            pickle.dump(self._model, f)
+
+    def load_model(self, path: str):
+        """
+        Load trained model from given path
+        """
+        with open(f"{path}/model.pkl", "rb") as f:
+            self._model = pickle.load(f)
+
     def predict(self, features: pd.DataFrame) -> List[int]:
         """
         Predict delays for new flights.
@@ -114,4 +138,50 @@ class DelayModel:
         Returns:
             (List[int]): predicted targets.
         """
-        return self._model.predict(features).tolist()
+        try:
+            pred = self._model.predict(features).tolist()
+        except Exception as e:
+            raise e
+
+        return pred
+
+
+if __name__ == "__main__":
+    from sklearn.metrics import classification_report, mean_squared_error
+    from sklearn.model_selection import train_test_split
+
+    # perform a training of the model for production deployment
+    model = DelayModel()
+    data = pd.read_csv(filepath_or_buffer="data/data.csv")
+
+    features, target = model.preprocess(data=data, target_column="delay")
+
+    _, features_validation, _, target_validation = train_test_split(
+        features, target, test_size=0.33, random_state=42
+    )
+
+    model.fit(features=features, target=target)
+
+    predicted_target = model.predict(features_validation)
+
+    report = classification_report(
+        target_validation, predicted_target, output_dict=True
+    )
+
+    import ipdb
+
+    ipdb.set_trace()
+
+    # save
+    model.save_model("challenge/models")
+
+    # re instantiate to override model
+    model = DelayModel()
+    model.load_model("challenge/models")
+
+    predicted_target_load = model.predict(features_validation)
+
+    print(
+        "The difference in prediction is:"
+        f" {mean_squared_error(predicted_target, predicted_target_load)}"
+    )
